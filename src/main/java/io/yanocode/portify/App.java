b5a8ed91e73319733de2cbe152335923911c3d6c
yassineDevBoa <yassine.rassy@taillislabs.com>
2024-02-29 17:32:38 +0100

    (Applicaiton): rename the main Class to App

--- src/main/java/io/yanocode/portify/App.java
+++ src/main/java/io/yanocode/portify/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PortifyApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(PortifyApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
