7c2cce2f3db90bdc8c2fed842f83094a1c4fe281
yassineDevBoa <yassine.rassy@taillislabs.com>
2024-02-29 17:31:37 +0100

    setup the project

---
+++ src/main/java/io/yanocode/portify/App.java
@@ -0,0 +1,13 @@
+package io.yanocode.portify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/yanocode/portify/PortifyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yanocode.portify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
